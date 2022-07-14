ea5ac1e9248bf04b345bb661f2b542a059c90239
IntegralTeam <sfxdx@sfxdx.com>
2022-07-14 12:27:15 +0300

    Add Hardhat Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { ethers } = require("ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {runs: 1, enabled: true},
+        },
+      },
+    ],
+  },
+  networks: {
+    // a.k.a localhost
+    hardhat: {
+      // Create 20 Signers with 1000 wei(not ETH!!!) each
+      accounts: {
+        count: 20,
+        accountsBalance: ethers.utils.parseEther('1000').toString(),
+      },
+    },
+  },
+  mocha: {
+    timeout: 20000000,
+  },
+  paths: {
+    sources: "./contracts/",
+    tests: "./test/",
+  },
 };
+
+
