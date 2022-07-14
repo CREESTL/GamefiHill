9d36b39084858b207bd427556224799307f09a7d
IntegralTeam <sfxdx@sfxdx.com>
2022-07-14 13:26:14 +0300

    Add More Transfer Tests

--- test/kraken_test.js
+++ test/kraken_test.js
@@ -2,7 +2,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-
 // Tests for Kraken.sol
 describe("Kraken", function () {
   
@@ -14,102 +13,106 @@ describe("Kraken", function () {
   let addr2;
   let addrs;
 
+  // Sample URI string
   let uri = "uri_string";
 
   // Hook
   beforeEach(async function () {
-
+    // Deploy token and get valid account before each test case
     _kraken = await ethers.getContractFactory("Kraken");
     kraken = await _kraken.deploy();
     await kraken.deployed();
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
   });
 
   describe("Deployment", function () {  
+
     it("Should be named Kraken", async function () {
       expect(await kraken.name()).to.equal("Kraken");
     });
 
-    it("Should have a Kraken symbol", async function () {
+    it("Should have a KRK symbol", async function () {
       expect(await kraken.symbol()).to.equal("KRK");
     });
-
 
   });
 
   describe("Transactions", function () {
 
     it("Should mint tokens to accounts", async function () {
+      // Mint a single token to account
       await kraken.connect(addr1).giveMeToken(uri);
       const addr1Balance = await kraken.balanceOf(addr1.address);
       expect(addr1Balance).to.equal(1);
     });
 
     it("Should not mint more than one token for address", async function () {
-      await kraken.giveToken(addr1.address, uri);
-      const addr1Balance = await kraken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(1);
-      await expect(kraken.giveToken(addr1.address, uri)).to.revertedWith("Only a Single Token for Address is Allowed!");
-    });    
+      // Mint the first token to account
+      await kraken.connect(addr1).giveMeToken(uri);
+      // Try mint the second token to account
+      await expect(kraken.connect(addr1).giveMeToken(uri)).to.revertedWith("Only a Single Token for Address is Allowed!");
+    });   
 
-    it("Should increment token ID after mint", async function () {
-      expect(addr1.address).to.not.be.equal(addr2.address);
-      await kraken.connectgiveToken(addr1.address, uri);
-      await kraken.giveToken(owner.address, uri);
-      const addr1Balance = await kraken.balanceOf(addr1.address);
+    it("Should transfer tokens between accounts", async function () {
+      // Mint one token to first account
+      await kraken.connect(addr1).giveMeToken(uri);
+      // First account's balance should be 1
+      // Second account's balance should be 0
+      let addr1BalanceInitial = await kraken.balanceOf(addr1.address);
+      expect(addr1BalanceInitial).to.equal(1);      
+      let addr2BalanceInitial = await kraken.balanceOf(addr2.address);
+      expect(addr2BalanceInitial).to.equal(0);
+      // Approve and transfer 1 token from first account to the second one
       await kraken.connect(addr1).approve(addr2.address, 0);
-      await kraken.connect(addr1).transferFrom(addr1.address, addr2.address, 1);
+      await kraken.connect(addr1).transferFrom(addr1.address, addr2.address, 0);
+      // First account's balance should be 0
+      // Second account's balance should be 1
+      let addr1BalanceFinal = await kraken.balanceOf(addr1.address);
+      expect(addr1BalanceFinal).to.equal(0);      
+      let addr2BalanceFinal = await kraken.balanceOf(addr2.address);
+      expect(addr2BalanceFinal).to.equal(1);
     });
 
 
-    it("Should transfer tokens between accounts", async function () {
-      await kraken.giveToken(addr1.address, uri);
-      const addr1Balance = await kraken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(1);
-      await kraken.connect(addr1).approve(addr2.address, 0);
-      await kraken.connect(addr1).transferFrom(addr1.address, addr2.address, 0);
-      const addr2Balance = await kraken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(1);
+    it("Should increment token ID after mint", async function () {
+      // Mint token with ID = 0
+      await kraken.connect(addr1).giveMeToken(uri);
+      // Mint token with ID = 1
+      await kraken.connect(addr2).giveMeToken(uri);
+      // Approve transfer of the latest minted token
+      // If ID was not incremented, this line should fail
+      await kraken.connect(addr2).approve(addr1.address, 1);
+      // Transfer latest minted token
+      await kraken.connect(addr2).transferFrom(addr2.address, addr1.address, 1);
     });
 
     it("Should fail transfer of token with incorrect ID", async function () {
-
-      await kraken.giveToken(addr1.address, uri);
+      // Mint token with ID = 0
+      await kraken.connect(addr1).giveMeToken(uri);
+      // Approve and transfer this token
       await kraken.connect(addr1).approve(addr2.address, 0);
       await kraken.connect(addr1).transferFrom(addr1.address, addr2.address, 0);
+      // Try to transfer token with ID = 777 (does not exist)
       await expect(kraken.connect(addr1).transferFrom(addr1.address, addr2.address, 777)).to.be.revertedWith("ERC721: invalid token ID");
     
     });
 
-    it("Should update balances after transfers", async function() {
-      await kraken.mint(owner.address, 10000);
-      const initialOwnerBalance = await kraken.balanceOf(owner.address);
+    it("Should have correct max total supply", async function () {
+      // Max total supply should be equal to 60 000
+      let maxTotalSupply = await kraken.getMaxTotalSupply();
+      expect(maxTotalSupply).to.equal(60_000);
+    });
 
-      expect(initialOwnerBalance).to.equal(10000);
-
-      // Transfer 100 tokens from owner to addr1
-      await kraken.approve(addr1.address, 100);
-      await kraken.transfer(addr1.address, 100);
-
-      // Transfer 500 tokens from owner to addr2
-      await kraken.approve(addr2.address, 500);
-      await kraken.transfer(addr2.address, 500);
-
-      // Check that final owner balance decreased by 600
-      const finalOwnerBalance = await kraken.balanceOf(owner.address);
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(600));
-
-      // Check that addr1 balance increased by 100
-      const addr1Balance = await kraken.balanceOf(addr1.address);
-      expect(addr1Balance).to.equal(100);
-
-      // Check that addr2 balance increased by 500
-      const addr2Balance = await kraken.balanceOf(addr2.address);
-      expect(addr2Balance).to.equal(500);
-
+    it("Should have correct total supply", async function () {
+      // Mint 3 tokens
+      await kraken.connect(addr1).giveMeToken(uri);
+      await kraken.connect(addr2).giveMeToken(uri);
+      await kraken.connect(addr3).giveMeToken(uri);
+      // Total supply should be equal to 3
+      let maxTotalSupply = await kraken.getTotalSupply();
+      expect(maxTotalSupply).to.equal(3);
     });
 
   });
-
 
 });
