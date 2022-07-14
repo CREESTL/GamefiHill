03309baf85dcb169beb560a12900e28787f779ef
IntegralTeam <sfxdx@sfxdx.com>
2022-07-14 13:27:00 +0300

    Add Total Supply and Max Total Supply. Add Getters for Them.

--- contracts/Kraken.sol
+++ contracts/Kraken.sol
@@ -6,39 +6,55 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract Kraken is ERC721URIStorage {
-    
-    // TODO totalSupply
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    // Idintifies if a user has minted a token
     mapping(address => bool) mintedTokens;
 
-    uint16 constant totalSupply = 60_000; 
+    // Maximum number of tokens to mint
+    uint16 constant public maxTotalSupply = 60_000; 
 
     constructor() ERC721("Kraken", "KRK") {}
 
     // Issues 1 token with a provided URI to the user
     // Returns new token's ID
-    // Can only be called by the 
     function giveMeToken(string memory tokenURI) public returns (uint256) {
 
         // Get the current latest ID
         // First ID is 0
         uint256 newItemId = _tokenIds.current();
-        // Check whether ID is greater than max supply
-        require(newItemId <= totalSupply, "Total Supply of Tokens Exceeded");
+        // Check whether ID is greater than max total supply
+        // If someone burns his token, it will not affect the number of minted tokens
+        require(newItemId <= maxTotalSupply, "Total Supply of Tokens Exceeded");
+
         // User can mint only one token
         require(!mintedTokens[msg.sender], "Only a Single Token for Address is Allowed!");
         mintedTokens[msg.sender] = true;
+
         // Mint token with the ID to the user
         _mint(msg.sender, newItemId);
+
         // Set the URI of the token
         _setTokenURI(newItemId, tokenURI);
+
         // Increment ID
         _tokenIds.increment();
 
         return newItemId;
+    }
+
+    // Getter for maxTotalSupply
+    // (max number of tokens to mint)
+    function getMaxTotalSupply() public pure returns (uint16) {
+        return maxTotalSupply;
+    }
+
+    // Getter for totalSupply
+    // (number of tokens minted)
+    function getTotalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 
     // TODO admin giveToken
