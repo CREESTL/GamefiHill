02b6e834b93953438e54d19e34f3d4dfbdf6f8c8
IntegralTeam <sfxdx@sfxdx.com>
2022-07-15 13:44:03 +0300

    Add Owner GiveTokenTo Function and Tests for It.

--- contracts/Kraken.sol
+++ contracts/Kraken.sol
@@ -18,7 +18,7 @@ contract Kraken is ERC721URIStorage, Ownable {
 
     constructor() ERC721("Kraken", "KRK") {}
 
-    // Issues 1 token with a provided URI to the user
+    // Issues 1 token with a provided URI to the caller
     // Returns new token's ID
     function giveMeToken(string memory tokenURI) public returns (uint256)  {
 
@@ -35,6 +35,34 @@ contract Kraken is ERC721URIStorage, Ownable {
 
         // Mint token with the ID to the user
         _mint(msg.sender, newItemId);
+
+        // Set the URI of the token
+        _setTokenURI(newItemId, tokenURI);
+
+        // Increment ID
+        _tokenIds.increment();
+
+        return newItemId;
+    }
+
+    // Issues 1 token with a provided URI to the user
+    // Returns new token's ID
+    // Can only be called by the owner of the contract!
+    function giveTokenTo(address user, string memory tokenURI) public onlyOwner returns (uint256)  {
+
+        // Get the current latest ID
+        // First ID is 0
+        uint256 newItemId = _tokenIds.current();
+        // Check whether ID is greater than max total supply
+        // If someone burns his token, it will not affect the number of minted tokens
+        require(newItemId <= maxTotalSupply, "Total Supply of Tokens Exceeded");
+
+        // Only one token for a user can be minted
+        require(!mintedTokens[user], "Only a Single Token for Address is Allowed!");
+        mintedTokens[user] = true;
+
+        // Mint token with the ID to the user
+        _mint(user, newItemId);
 
         // Set the URI of the token
         _setTokenURI(newItemId, tokenURI);
