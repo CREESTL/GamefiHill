7e04c77a5c42f389ba8296f66a396c621200f8f3
IntegralTeam <sfxdx@sfxdx.com>
2022-07-18 13:26:34 +0300

    Add NatSpec Comments

--- contracts/Kraken.sol
+++ contracts/Kraken.sol
@@ -5,82 +5,83 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title A user avatar NFT token
 contract Kraken is ERC721URIStorage, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // Idintifies if a user has minted a token
+    /// @dev Idintifies if a user has minted a token
     mapping(address => bool) mintedTokens;
 
-    // Maximum number of tokens to mint
+    /// @dev Maximum number of tokens to mint
     uint256 constant public maxTotalSupply = 6_000_000; 
 
     constructor() ERC721("Kraken", "KRK") {}
 
-    // Issues 1 token with a provided URI to the caller
-    // Returns new token's ID
+    /// @notice Issues 1 token with a provided URI to the caller
+    /// @notice Returns new token's ID
+    /// @param tokenURI The URI of item in the IPFS storage
+    /// @return ID of the new token
     function giveMeToken(string memory tokenURI) public returns (uint256)  {
 
-        // Get the current latest ID
-        // First ID is 0
+        /// @dev Get the current latest ID. Starts with 0.
         uint256 newItemId = _tokenIds.current();
-        // Check whether ID is greater than max total supply
-        // If someone burns his token, it will not affect the number of minted tokens
+        /// @dev Check whether ID is greater than max total supply
+        /// @notice If someone burns his token, it will not affect the number of minted tokens
         require(newItemId <= maxTotalSupply, "Total Supply of Tokens Exceeded");
 
-        // User can mint only one token
+        /// @notice User can mint only one token
         require(!mintedTokens[msg.sender], "Only a Single Token for Address is Allowed!");
         mintedTokens[msg.sender] = true;
 
-        // Mint token with the ID to the user
+        /// @dev Mint token with the ID to the user
         _mint(msg.sender, newItemId);
 
-        // Set the URI of the token
+        /// @dev Set the URI of the token
         _setTokenURI(newItemId, tokenURI);
 
-        // Increment ID
+        /// @dev Increment token's ID
         _tokenIds.increment();
 
         return newItemId;
     }
 
-    // Issues 1 token with a provided URI to the user
-    // Returns new token's ID
-    // Can only be called by the owner of the contract!
+    /// @notice Issues 1 token with a provided URI to the provided address
+    /// @notice Returns new token's ID
+    /// @notice Can only be called by the owner of the contract
+    /// @param tokenURI The URI of item in the IPFS storage
+    /// @return ID of the new token
     function giveTokenTo(address user, string memory tokenURI) public onlyOwner returns (uint256)  {
 
-        // Get the current latest ID
-        // First ID is 0
+        /// @dev Get the current latest ID. Starts with 0.
         uint256 newItemId = _tokenIds.current();
-        // Check whether ID is greater than max total supply
-        // If someone burns his token, it will not affect the number of minted tokens
+        /// @dev Check whether ID is greater than max total supply
+        /// @notice If someone burns his token, it will not affect the number of minted tokens
         require(newItemId <= maxTotalSupply, "Total Supply of Tokens Exceeded");
 
-        // Only one token for a user can be minted
+        /// @notice Only one token for a user can be minted
         require(!mintedTokens[user], "Only a Single Token for Address is Allowed!");
         mintedTokens[user] = true;
 
-        // Mint token with the ID to the user
+        /// @dev Mint token with the ID to the user
         _mint(user, newItemId);
 
-        // Set the URI of the token
+        /// @dev Set the URI of the token
         _setTokenURI(newItemId, tokenURI);
 
-        // Increment ID
+        /// @dev Increment token's ID
         _tokenIds.increment();
 
         return newItemId;
     }
 
-    // Getter for maxTotalSupply
-    // (max number of tokens to mint)
+    /// @notice Get the maximum supply of tokens
     function maxTotalSupply() public pure returns (uint256) {
         return maxTotalSupply;
     }
 
-    // Getter for totalSupply
-    // (number of tokens minted)
+    /// @notice Get the current supply of tokens
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
