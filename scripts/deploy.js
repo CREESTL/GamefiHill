4da620cb30b701422d2811082a76c4aa0fccd01b
IntegralTeam <sfxdx@sfxdx.com>
2022-07-21 13:50:16 +0300

    Upgrade Deploy Script. Add Verification to It.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,53 @@
 // SPDX-License-Identifier: MIT 
-const hre = require("hardhat");
+const { ethers, network } = require("hardhat");
+const fs = require("fs");
+const path = require("path");
+const delay = require("delay");
+
+// JSON file to keep information about previous deployments
+const OUTPUT_DEPLOY = require("./deployOutput.json");
 
 async function main() {
+
+  console.log("Start of Deployment...");
 
   // Get different avaliable addresses
   [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
   
-  // We get the contract to deploy
-  // Deploy the token
+  // Get the contract and deploy it
   _kraken = await ethers.getContractFactory("Kraken");
-  kraken = await _kraken.deploy();
-  await kraken.deployed();
+  krakenTx = await _kraken.deploy();
+  let kraken = await krakenTx.deployed();
 
-  console.log("Kraken deployed to:", kraken.address);
+  console.log("Deployment Finished!");
+  console.log("Start of Verification...");
+  
+  // Sleep for 90 seconds, otherwise blockexplorer will fail
+  await delay(90000);
+  
+  // Verify the contract
+  // Provides all contract's dependencies as separate files
+  try { 
+    await hre.run("verify:verify", {
+      address: kraken.address,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log("Verification Finished!");
+  console.log(`See Results in "${__dirname + '/deployOutput.json'}" File`);
+
+  // Write output to the JSON file
+  OUTPUT_DEPLOY.networks[network.name].address = kraken.address;
+  OUTPUT_DEPLOY.networks[network.name].verification =
+    "https://mumbai.polygonscan.com/address/" + kraken.address + "#code";
+  fs.writeFileSync(
+    path.resolve(__dirname, "./deployOutput.json"),
+    JSON.stringify(OUTPUT_DEPLOY, null, "  ")
+  );
 }
+
 
 main()
   .then(() => process.exit(0))
